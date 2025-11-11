681e65d1a24689f76e9e5c17c030b382b1338c0f
TalithaSyakirah <talithaskrh06@gmail.com>
2025-11-11 13:28:45 +0700

    Visualisasi

--- app/Http/Controllers/visualisasiController.php
+++ app/Http/Controllers/visualisasiController.php
@@ -36,10 +36,27 @@ class visualisasiController extends Controller
         $DataIPMMenurutKabupatenKotaDiKalimantanBarat = Data_IPM_Menurut_Kabupaten_Kota_Di_Kalimantan_Barat::all();
         $DataPertumbuhanNilaiTambahKategoriKotaPontianak = Data_Pertumbuhan_Nilai_Tambah_Kategori_Kota_Pontianak::all();
         $columnsDataPertumbuhanNilaiTambahKategoriKotaPontianak = array_diff(Schema::getColumnListing('data__pertumbuhan__nilai__tambah__kategori__kota__pontianaks'), ['id', 'Tahun', 'created_at', 'updated_at']);
-        $DataKontribusiKategoriTerhadapPDRBKotaPontianak = Data_Kontribusi_Kategori_Terhadap_PDRB_Kota_Pontianak::all();
+        $DataKontribusiKategoriTerhadapPDRBKotaPontianak = Data_Kontribusi_Kategori_Terhadap_PDRB_Kota_Pontianak::orderBy('Tahun', 'asc')->get();
         $columnsDataKontribusiKategoriTerhadapPDRBKotaPontianak = array_values(array_filter(Schema::getColumnListing('data__kontribusi__kategori__terhadap__p_d_r_b__kota__pontianaks'),fn($col) => !in_array($col, ['id', 'Tahun', 'created_at', 'updated_at', 'Jumlah' ])));
         $DataTingkatInflasiKotaPontianak = Data_Tingkat_Inflasi_Kota_Pontianak::all();
-       
+
+        $kontribusiDataKeys = [
+            'Pertanian_Kehutanan_dan_Perikanan', 'Pertambangan_dan_Penggalian', 'Industri_Pengolahan',
+            'Pengadaan_Listrik_dan_Gas', 'Pengadaan_Air_Pengelolaan_Sampah_Limbah_dan_Daur_Ulang', 'Konstruksi',
+            'Perdagangan_Besar_dan_Eceran_Reparasi_Mobil_dan_Sepeda_Motor', 'Transportasi_dan_Pergudangan',
+            'Penyediaan_Akomodasi_dan_Makan_Minuman', 'Informasi_dan_Komunikasi',
+            'Jasa_Keuangan_dan_Asuransi', 'Real_Estate', 'Jasa_Perusahaan',
+            'Administrasi_Pemerintahan_Pertahanan_dan_Jaminan_Sosial_Wajib',
+            'Jasa_Pendidikan', 'Jasa_Kesehatan_dan_Kegiatan_Sosial', 'Jasa_Lainnya'
+        ];
+
+        $kontribusiLabels = array_map(function($key, $index) {
+            // Ganti _ dengan spasi dan buat lebih rapi
+            $cleanLabel = str_replace(['_', 'dan_Perikanan', 'Reparasi_Mobil_dan_Sepeda_Motor', 'Limbah_dan_Daur_Ulang', 'Pertahanan_dan_Jaminan_Sosial_Wajib'], '', $key);
+            $cleanLabel = str_replace(['_'], ' ', $cleanLabel);
+            return ($index + 1) . '. ' . $cleanLabel;
+        }, $kontribusiDataKeys, array_keys($kontribusiDataKeys));
+
 
         return view('visualisasi', [
             'DataLuasWilayahPerKecamatan' => $dataLuasWilayahPerKecamatan,
@@ -66,6 +83,9 @@ class visualisasiController extends Controller
             'columnsDataKontribusiKategoriTerhadapPDRBKotaPontianak'=> $columnsDataKontribusiKategoriTerhadapPDRBKotaPontianak,
             'DataTingkatInflasiKotaPontianak' => $DataTingkatInflasiKotaPontianak,
             'columnsDataTingkatInflasiKotaPontianak',
+            'DataKontribusiKategoriTerhadapPDRBKotaPontianak',
+            'kontribusiLabels' => $kontribusiLabels,
+            'kontribusiDataKeys' => $kontribusiDataKeys,
         ]);
     }
 }
